e1e6e975a8fbdd451d0ff402e5b9dd321edd4557
yueqingxuan <37060585+yueqingxuan@users.noreply.github.com>
2018-04-25 15:24:12 +1000

    add new method: getUNSWMember()

--- NFEapp/models/EMS.py
+++ NFEapp/models/EMS.py
@@ -1,5 +1,5 @@
-class EMS(object):
-    def __init__():
+class EMS:
+    def __init__(self):
         self._openEvent = []
         self._UNSWMemeber = []
 
@@ -8,7 +8,12 @@ class EMS(object):
     
     def removeOpenEvent(self,event):
         self._openEvent.remove(event)
-    '''
+        
     def addUNSWMember(self, member):
         self._UNSWMember.append(member)
-    '''
+        
+    def getUNSWMember(self, name):
+        for person in self._UNSWMember:
+            if person.name == name:
+                return person
+        return None

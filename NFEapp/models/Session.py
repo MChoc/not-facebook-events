56b6cfbbe2cae4a8b3c4d8b970b161eb73c72e2f
yueqingxuan <z5042620@ad.unsw.edu.au>
2018-04-26 22:08:41 +1000

    my testing

--- NFEapp/models/Session.py
+++ NFEapp/models/Session.py
@@ -1,11 +1,42 @@
-class Session(Seminar):
+from Seminar import *
 
-    def __init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, session, speaker):
-        Seminar.__init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, session)
+class Session:
+    def __init__(self, name, date, time, location, maxAttendees, deRegWindow, abstractInfo, sessionStatus, speaker):
+        self._name = name
+        self._date = date
+        self._time = time
+        self._location = location
+        self._maxAttendees = maxAttendees
+        self._deRegWindow = deRegWindow
+        self._abstractInfo = abstractInfo
+        self._sessionStatus = sessionStatus       
         self._speaker = speaker
+        self._type = "session"
+        self._attendeeList = []
 
+    def get_name(self):
+	    return self._name
+
+    def get_type(self):
+	    return self._type
+	
     def get_speaker(self):
-        return self._speaker
+        return self._speaker.get_name()
 
     def set_speaker(self, speaker):
         self._speaker = speaker
+    
+    def get_attendeeList(self):
+        return self._attendeeList
+	
+    def add_attendee(self, member):
+        self._attendeeList.append(member)
+    
+    def remove_attendee(self, member):
+        self._attendeeList.remove(member)
+    
+    def get_sessionStatus(self):
+        return self._sessionStatus
+    
+    def set_sessionStatus(self, status):
+        self._sessionStatus = status

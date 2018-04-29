d6ce360d3c60ea563d0bd9430464e4b430758c90
MChoc <colin.yang96@gmail.com>
2018-04-29 14:50:05 +1000

    added attr: id, name. added get/set

--- NFEapp/models/Seminar.py
+++ NFEapp/models/Seminar.py
@@ -1,11 +1,18 @@
 class Seminar(Event):
 
-    def __init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, session):
-        Event.__init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo)
+    def __init__(self, id, name, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo):
+        Event.__init__(self, id, name, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo)
         self._session = []
 
-    def get_session(self):
+
+##
+    # return list[session]
+    @property
+    def sessions(self):
         return self._session
 
+    # adds session to session list in Seminar
+    # arg1 session
     def add_session(self, session):
         self._session.append(session)
+##

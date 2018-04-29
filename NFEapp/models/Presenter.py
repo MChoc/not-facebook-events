44cef5f744ff7ab7f3371f064361486810ab4282
yueqingxuan <z5042620@ad.unsw.edu.au>
2018-04-29 16:04:49 +1000

    using property as getter and setter

--- NFEapp/models/Presenter.py
+++ NFEapp/models/Presenter.py
@@ -1,16 +1,6 @@
-#from Convenor import *
+from Staff import *
 #not using
-class Presenter(object):
+class Presenter(Staff):
+    pass
 
-    def __init__(self, name, contact):
-        self._name = name
-        self._contact = contact #email
-
-    def get_name(self):
-        return self._name
-
-    def get_contact(self):
-        return self._contact
-    
-    def __str__(self):
-        return "Speaker name: {0}, contact: {1}".format(self._name, self._contact)
+#do we actually need convenor and presenter
--- NFEapp/models/Speaker.py
+++ NFEapp/models/Speaker.py
@@ -4,11 +4,21 @@ class Speaker(object):
         self._name = name
         self._contact = contact #email
 
-    def get_name(self):
+     @property
+    def name(self):
         return self._name
 
-    def get_contact(self):
+    @property
+    def contact(self):
         return self._contact
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @contact.setter
+    def contact(self, contact):
+        self._contact = contact
     
     def __str__(self):
         return "Speaker name: {0}, contact: {1}".format(self._name, self._contact)

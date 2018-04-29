1d37a38b8ec56cbe92baa0fa5ba29028f801c914
MChoc <colin.yang96@gmail.com>
2018-04-29 14:55:53 +1000

    added attr: id, name. added get/set for speaker

--- NFEapp/models/Session.py
+++ NFEapp/models/Session.py
@@ -1,11 +1,19 @@
-class Session(Seminar):
+class Session(Event):
 
-    def __init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, session, speaker):
-        Seminar.__init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, session)
+    def __init__(self, id, name, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo, speaker):
+        Seminar.__init__(self, status, date, time, location, attendeeList, maxAttendees, deRegWindow, abstractInfo)
         self._speaker = speaker
 
-    def get_speaker(self):
+
+##
+    # return str: speaker for session
+    @property
+    def speaker(self):
         return self._speaker
 
-    def set_speaker(self, speaker):
+    # changes speaker
+    # arg1 Speaker
+    @speaker.setter
+    def speaker(self, speaker):
         self._speaker = speaker
+##

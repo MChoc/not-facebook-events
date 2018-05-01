f75b1b24e0cdde6dec41feb0173e3269f1187a31
MChoc <colin.yang96@gmail.com>
2018-05-01 15:06:36 +1000

    added system id counter

--- NFEapp/models/EMS.py
+++ NFEapp/models/EMS.py
@@ -6,28 +6,35 @@ from Convenor import *
 class EMS:
     def __init__(self):
         self._openEvent = []
+        self._openEventCounter = 0
         self._UNSWMember = []
+
+    # system id counter for events
+    @property
+    def counter(self):
+        return self._openEventCounter
 
     def addOpenEvent(self, event):
         self._openEvent.append(event)
-    
+        self._openEventCounter += 1
+
     def removeOpenEvent(self,event):
         self._openEvent.remove(event)
-    
+
     def addUNSWMember(self, member):
         self._UNSWMember.append(member)
-    
+
     def getUNSWMember(self, name):
         for person in self._UNSWMember:
             if person.name == name:
                 return person
         return None
-    
+
     def getOpenEvent(self, name):
         for event in self._openEvent:
             if event.name == name:
                 return event
         return None
-    
+
     def get_all_open_event(self):
         return self._openEvent

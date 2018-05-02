c815a2e29aa31ea80c3ab09d78ab2a77d3389c17
yueqingxuan <z5042620@ad.unsw.edu.au>
2018-05-03 03:05:53 +1000

    use werkzeug.security

--- NFEapp/models/Member.py
+++ NFEapp/models/Member.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from abc import ABC, abstractmethod
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Member(UserMixin, ABC):
     __id = -1
@@ -7,8 +8,8 @@ class Member(UserMixin, ABC):
     def __init__(self, username, password):
         self._id = self._generate_id()
         self._username = username
-        self._password = password
-
+        self._password = generate_password_hash(password)
+    
     @property
     def username(self):
         return self._username
@@ -31,5 +32,5 @@ class Member(UserMixin, ABC):
     def _generate_id(self):
         Member.__id += 1
 
-    def validate_password(self, password):
-        return self._password == password
+    def check_password(self, password):
+        return check_password_hash(self._password, password)

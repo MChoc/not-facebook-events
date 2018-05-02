2d08ae3a9ea63f8948baeb6a66cdd5893183b8e9
MChoc <colin.yang96@gmail.com>
2018-05-02 13:47:24 +1000

    delete unecessary files

--- NFEapp/models/Presenter.py
+++
@@ -1,6 +0,0 @@
-from Staff import *
-#not using
-class Presenter(Staff):
-    pass
-
-#do we actually need convenor and presenter
--- testFile.py
+++
@@ -1,5 +0,0 @@
-from NFEapp/models/Event import *
-from NFEapp/models/Course import *
-from NFEapp/models/Session import *
-from NFEapp/models/Seminar import *
-from NFEapp/models/Speaker import *

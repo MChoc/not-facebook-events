e6f9c179cda55803bc9e074234e5509dc8259762
yueqingxuan <z5042620@ad.unsw.edu.au>
2018-05-03 01:48:51 +1000

    hash password

--- NFEapp/models/bootstrap_system.py
+++ NFEapp/models/bootstrap_system.py
@@ -1,10 +1,28 @@
 import csv
 from EMS import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def bootstrap_system():
     system = EMS()
+    row_list = []
+    with open("user.csv", 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        for row in reader:
+            username = row[0]
+            zID = row[1]
+            email = row[2]
+            passw = row[3]
+            password = generate_password_hash(passw)
+            row[3]= password
+            #writer.writerow(row[3], password)
+            role = row[4]
+            row_list.append(row)
 
-    with open("user.csv", "r") as file:
+    with open("user.csv", 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(row_list)
+        
+    with open("user.csv", 'r') as file:
         reader = csv.reader(file, delimiter=',')
         for row in reader:
             username = row[0]
@@ -16,5 +34,6 @@ def bootstrap_system():
                 system.addUNSWMember(Staff(username, zID, email, password, role))
             elif role == 'trainee':
                 system.addUNSWMember(Student(username, zID, email, password, role))
-        
+            
+    
     return system

88138f9ee15ac834e863e54db71951572401ea70
MChoc <colin.yang96@gmail.com>
2018-05-27 22:08:49 +1000

    editted pytest

--- NFEapp/test_register_seminar.py
+++ NFEapp/test_register_seminar.py
@@ -1,5 +1,6 @@
 import unittest
 import pytest
+import datetime
 from server import system
 from models.Seminar import *
 from models.EMS import *
@@ -14,6 +15,8 @@ class RegisterSeminarTestCase(unittest.TestCase):
         self.seminar = system.getOpenEvent('New College presents: How to Succeed in Business Without Really Trying')
         self.session1 = self.seminar.get_one_session('Session 1')
         self.session2 = self.seminar.get_one_session('Session 2')
+        self.seminarE = system.getOpenEvent('COMP1531')
+        self.sessionE1 = self.seminarE.get_one_session('Lesson 1')
         self.staff = system.getUNSWMember('name4119988')
         self.student = system.getUNSWMember('name6119988')
         self.placeholder = system.getUNSWMember('name6119989')
@@ -84,26 +87,26 @@ class RegisterSeminarTestCase(unittest.TestCase):
     # Early bird window will need to be manually changed to fit realtime date
 
     # Guest registers for an event before early bird date and pays early bird rate
-    # @pytest.mark.run(order=7)
-    # def test_guest_register_earlyFee(self):
-    #     assert system.register_seminar(self.guest, self.seminar, self.session1) == None
-    #     self.assertEqual(system.check_reg_fee(self.guest, self.session1), 10)
-    #     system.deRegister_seminar(self.guest, self.seminar)
-    #     assert len(self.session1.attendeeList) == 0
-
-    # Guest registers for an event after early bird date and pays full fee
     @pytest.mark.run(order=7)
-    def test_guest_register_fee(self):
+    def test_guest_register_earlyFee(self):
         assert system.register_seminar(self.guest, self.seminar, self.session1) == None
-        self.assertEqual(system.check_reg_fee(self.guest, self.session1), 20)
+        self.assertEqual(system.check_reg_fee(self.guest, self.session1), 10)
         system.deRegister_seminar(self.guest, self.seminar)
         assert len(self.session1.attendeeList) == 0
+
+    # Guest registers for an event after early bird date and pays full fee
+    @pytest.mark.run(order=8)
+    def test_guest_register_fullFee(self):
+        assert system.register_seminar(self.guest, self.seminarE, self.sessionE1) == None
+        self.assertEqual(system.check_reg_fee(self.guest, self.sessionE1), 20)
+        system.deRegister_seminar(self.guest, self.seminarE)
+        assert len(self.sessionE1.attendeeList) == 0
 
 ##
 ## Seminar closed test cases
 
     # Students cannot register for a closed seminar
-    @pytest.mark.run(order=8)
+    @pytest.mark.run(order=9)
     def test_student_register_seminar_closed(self):
         self.seminar.status = 'closed'
         self.session1.status = 'closed'
@@ -113,7 +116,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Staff cannot register for a closed seminar
-    @pytest.mark.run(order=9)
+    @pytest.mark.run(order=10)
     def test_staff_register_seminar_closed(self):
         assert self.seminar.status == 'closed'
         assert self.session1.status == 'closed'
@@ -123,7 +126,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Guests cannot register for a closed seminar
-    @pytest.mark.run(order=10)
+    @pytest.mark.run(order=11)
     def test_guest_register_seminar_closed(self):
         assert self.seminar.status == 'closed'
         assert self.session1.status == 'closed'
@@ -140,7 +143,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
     # Testing session1 closed
 
     # Students cannot register for a closed session
-    @pytest.mark.run(order=11)
+    @pytest.mark.run(order=12)
     def test_student_register_session_closed(self):
         self.session1.status = 'closed'
         assert system.register_seminar(self.student, self.seminar, self.session1) == False
@@ -148,7 +151,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Staff cannot register for a closed session
-    @pytest.mark.run(order=12)
+    @pytest.mark.run(order=13)
     def test_staff_register_session_closed(self):
         assert self.session1.status == 'closed'
         assert system.register_seminar(self.staff, self.seminar, self.session1) == False
@@ -156,7 +159,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Guests cannot register for a closed session
-    @pytest.mark.run(order=13)
+    @pytest.mark.run(order=14)
     def test_guest_register_session_closed(self):
         assert self.session1.status == 'closed'
         assert system.register_seminar(self.guest, self.seminar, self.session1) == False
@@ -169,7 +172,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
     # Testing session1 full
 
     # Students cannot register for a full session
-    @pytest.mark.run(order=14)
+    @pytest.mark.run(order=15)
     def test_student_register_session_full(self):
         system.register_seminar(self.placeholder, self.seminar, self.session1)
         assert system.register_seminar(self.student, self.seminar, self.session1) == False
@@ -177,14 +180,14 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 1
 
     # Staff cannot register for a full session
-    @pytest.mark.run(order=15)
+    @pytest.mark.run(order=16)
     def test_staff_register_session_full(self):
         assert self.session1.attendeeList[0] == self.placeholder
         system.register_seminar(self.staff, self.seminar, self.session1) == False
         system.deRegister_seminar(self.staff, self.seminar)
         assert len(self.session1.attendeeList) == 1
 
-    @pytest.mark.run(order=16)
+    @pytest.mark.run(order=17)
     def test_guest_register_session_full(self):
         assert self.session1.attendeeList[0] == self.placeholder
         system.register_seminar(self.guest, self.seminar, self.session1) == False
@@ -196,7 +199,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
 ## User already registered test cases
 
     # Students cannot register for a session they are already registered for
-    @pytest.mark.run(order=17)
+    @pytest.mark.run(order=18)
     def test_student_register_session_already(self):
         system.register_seminar(self.student, self.seminar, self.session1)
         assert system.register_seminar(self.student, self.seminar, self.session1) == False
@@ -204,7 +207,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Staff cannot register for a session they are already registered for
-    @pytest.mark.run(order=18)
+    @pytest.mark.run(order=19)
     def test_staff_register_session_already(self):
         system.register_seminar(self.staff, self.seminar, self.session1)
         assert system.register_seminar(self.staff, self.seminar, self.session1) == False
@@ -212,7 +215,7 @@ class RegisterSeminarTestCase(unittest.TestCase):
         assert len(self.session1.attendeeList) == 0
 
     # Guests cannot register for a session they are already registered for
-    @pytest.mark.run(order=19)
+    @pytest.mark.run(order=20)
     def test_guest_register_session_already(self):
         system.register_seminar(self.guest, self.seminar, self.session1)
         assert system.register_seminar(self.guest, self.seminar, self.session1) == False

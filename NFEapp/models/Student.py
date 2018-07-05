c31ddea274b21e75bd35be317a89d91bca826278
MChoc <colin.yang96@gmail.com>
2018-07-05 19:17:33 +1000

    move

--- NFEapp/models/Student.py
+++
@@ -1,9 +0,0 @@
-from .UNSWMember import *
-
-class Student(UNSWMember):
-
-    def __init__(self, username, zID, email, password, role):
-        UNSWMember.__init__(self, username, zID, email, password, role)
-
-    def is_admin(self):
-        return False
--- NFEapp/test_all.py
+++
@@ -1,5 +0,0 @@
-from test_create_seminar import CreateSeminarTestCase
-from test_register_seminar import RegisterSeminarTestCase
-
-if __name__=='__main__':
-    unittest.main()
